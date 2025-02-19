f6182baf6eae6ec6d24be31ccb39b26c01ac2b95
DogGuyMan <escatrgot@gmail.com>
2025-02-19 15:17:50 +0900

    [Update] : Action And Main

--- scripts/main.cs
+++ scripts/main.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 public class Program
 {
     public static void Main(string[] args) {
-        // Udemy.MultiThreading.Lecture2.Major_1.MajorAction();
-        // Inflearn.MultiThreading.Lecture2.ThreadStartJoin.MajorAction(args);
-        // MyExtra.MultiThreading.IOOnWorkerThread.Major.MajorAction();
-        // MyExtra.MultiThreading.IOOnIOThread.Major.MajorAction();
-        Inflearn.MultiThreading.Lecture4.GuardedSuspension.MajorAction();
+        if(args[1] == "Lecture") {
+            Console.WriteLine("Lecture Run");
+            // Udemy.MultiThreading.Lecture2.Major_1.MajorAction();
+            // Inflearn.MultiThreading.Lecture2.ThreadStartJoin.MajorAction(args);
+            // MyExtra.MultiThreading.IOOnWorkerThread.Major.MajorAction();
+            // MyExtra.MultiThreading.IOOnIOThread.Major.MajorAction();
+            // Inflearn.MultiThreading.Lecture4.GuardedSuspension.MajorAction();
+        }
+        if(args[1] == "Test") {
+            Console.WriteLine("Test Run");
+        }
     }
 }
